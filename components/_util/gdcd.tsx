73bab6925811293388363c15f2a2af4fc15e4b62
李凤宝 <lifb@glodon.com>
2022-09-15 14:53:21 +0800

    feat: 修复copyWithStatic类型定义

--- components/_util/gdcd.tsx
+++ components/_util/gdcd.tsx
@@ -1,20 +1,15 @@
-import type { ComponentType, ForwardRefExoticComponent } from 'react';
-
 const ignoreKeys = ['$$typeof', 'render', 'contextType', 'contextTypes'];
 const mergeKeys = ['defaultProps', 'propTypes'];
 const unoverridableKeys = ['displayName', 'getDerivedStateFromProps', 'getDerivedStateFromError'];
 
 /** 把一个Antd组件的所有静态属性拷贝到新组件中 */
-// eslint-disable-next-line import/prefer-default-export
-export const copyWithStatic = <
-  PP,
-  CP extends PP,
-  TT extends ComponentType<CP> | ForwardRefExoticComponent<CP>,
-  TS extends ComponentType<PP> | ForwardRefExoticComponent<PP>,
->(
+export const copyWithStatic = <TS, TT>(
   source: TS,
   target: TT,
 ): TT & { [key in keyof TS]: TS[key] } => {
+  if (!source || !target) {
+    return target as TT & TS;
+  }
   Object.keys(source).forEach(key => {
     if (ignoreKeys.includes(key)) {
       return;
